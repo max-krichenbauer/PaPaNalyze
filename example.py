a0a3f2403bec3777f192f218c37bed1622daff47
Max Krichenbauer <max@marui-plugin.com>
2019-07-21 13:03:08 +0900

    Renamed "test.py" to "example.py" for more clarity.

--- example.py
+++ example.py
@@ -2,6 +2,7 @@
 import msr_parser
 import subscriptions_analyzer
 
-[subscriptions, months] = msr_parser.parse_msr_files('D:/Dropbox/biz/paypal/', reporting_currency='USD')
+# Parse data from PayPal monthly sales reports (MSR) files:
+[subscriptions, months] = msr_parser.parse_msr_files('C:/MSR files folder/', reporting_currency='USD')
 # Analyze and plot results:
 subscriptions_analyzer.plot_subscriptions(subscriptions, months)
